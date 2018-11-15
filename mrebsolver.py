6f0a7d07c4bd281e74661efa43f258f8ba8ec5fe
Roman Chefranov <roman_chefr@bk.ru>
2018-11-15 15:36:37 +0300

    Calculation refactoring

--- mrebsolver.py
+++ mrebsolver.py
@@ -1,5 +1,4 @@
 import re
-import copy
 
 
 class CodedDigit:
@@ -29,7 +28,7 @@ class CodedDigitFactory:
             return symbol
         if symbol in self.cache:
             return self.cache.get(symbol)
-        code_digit = CodedDigit()  # refactor default?
+        code_digit = CodedDigit()
         self.cache[symbol] = code_digit
         return code_digit
 
@@ -45,6 +44,7 @@ class CodedNumber:
         self._coded_digits = list(map(lambda symbol:
                                   coded_digit_factory.get(symbol),
                                   coded_digits))
+        self.is_positive = True
         coded_digit = self._coded_digits[0]
         if isinstance(coded_digit, CodedDigit):
             coded_digit.not_null = True  # First digit of number cannot be 0
@@ -53,14 +53,15 @@ class CodedNumber:
         str_digit = ''
         for coded_digit in self._coded_digits:
             str_digit += str(coded_digit)
-        return int(str_digit)
+        return int(str_digit) if self.is_positive else -int(str_digit)
 
     def __repr__(self):
         return ''.join(map(lambda digit: str(digit), self._coded_digits))
 
 
 class CodedExpression:
-    """Represents the coded expression"""
+    """Represents the coded expression. Calculation algorithm is taken from
+    R.Lafore. "Data Structures and Algorithms in Java" and needs optimization"""
 
     def __init__(self, coded_expression, factory):
         """
@@ -68,22 +69,35 @@ class CodedExpression:
         e.g. SEND + MORE
         :param factory: Coded Digit Factory
         """
-        self._items = re.split('(\W+)', coded_expression)
+        items = re.split('(\W)', coded_expression)
+        items = list(filter(lambda s: len(s) > 0, items))
         self._items = list(map(lambda item: CodedNumber(item, factory) if
                            item[0].isalnum() else item,
-                           self._items))
+                           items))
+        self._infix_items = []
+        self._parse()
+        self._stack = []
 
-    def __int__(self):  # todo full refactor
+    def __int__(self):
         if len(self._items) == 1:
             return int(self._items[0])
-        if self._items[1] == '+':
-            return int(self._items[0]) + int(self._items[2])
-        elif self._items[1] == '-':
-            return int(self._items[0]) - int(self._items[2])
-        elif self._items[1] == '*':
-            return int(self._items[0]) * int(self._items[2])
-        else:
-            return int(self._items[0]) / int(self._items[2])
+        stack = self._stack
+        for item in self._infix_items:
+            if isinstance(item, CodedNumber):
+                stack.append(int(item))
+            else:
+                num2 = stack.pop()
+                num1 = stack.pop()
+                if item == '+':
+                    result = num1 + num2
+                elif item == '-':
+                    result = num1 - num2
+                elif item == '*':
+                    result = num1 * num2
+                else:
+                    result = int(num1 / num2)
+                stack.append(result)
+        return stack.pop()
 
     def __repr__(self):
         str_expr = ''
@@ -92,6 +106,66 @@ class CodedExpression:
                 str_expr += ' '
             str_expr += str(self._items[i])
         return str_expr
+
+    def _parse(self):
+        if len(self._items) == 1:
+            self._infix_items = self._items
+            return
+        stack = []
+        for idx, item in enumerate(self._items):
+            if idx == 0 and not isinstance(item, CodedNumber):
+                if item == '-' and isinstance(self._items[1], CodedNumber):
+                    self._items[1].is_positive = False
+                    continue
+                if item != '(':
+                    raise IOError('Incorrect equation.'
+                                  'Incorrect or redundant operator ' +
+                                  item + ' at the beginning of an expression')
+            if not isinstance(item, CodedNumber):
+                if item == '+' or item == '-':
+                    self._parse_operator(item, 1, stack)
+                elif item == '*' or item == '/':
+                    self._parse_operator(item, 2, stack)
+                elif item == '(':
+                    stack.append(item)
+                elif item == ')':
+                    self._parse_brackets(stack)
+                else:
+                    raise IOError('Incorrect equation. Unknown operator "' + item + '".')
+            else:
+                self._infix_items.append(item)
+        while len(stack) > 0:
+            item = stack.pop()
+            if item == '(':
+                raise IOError('Incorrect equation. incorrect brackets placement.')
+            self._infix_items.append(item)
+
+    def _parse_operator(self, operator, priority, stack):
+        while len(stack) > 0:
+            prev_item = stack.pop()
+            if prev_item == '(':
+                stack.append(prev_item)
+                break
+            else:
+                if prev_item == '+' or prev_item == '-':
+                    prev_item_priority = 1
+                else:
+                    prev_item_priority = 2
+                if prev_item_priority < priority:
+                    stack.append(prev_item)
+                    break
+                else:
+                    self._infix_items.append(prev_item)
+        stack.append(operator)
+
+    def _parse_brackets(self, stack):
+        while len(stack) > 0:
+            item = stack.pop()
+            if item == '(':
+                break
+            self._infix_items.append(item)
+        else:
+            raise IOError('Incorrect equation. incorrect brackets placement.')
 
 
 class MathRebusSolver:
@@ -128,9 +202,16 @@ class MathRebusSolver:
                 continue
             coded_digit.digit = i
             if self._layer == self._coded_digits_len - 1:
-                if int(self._parts[0]) == int(self._parts[1]):
-                    yield MathRebusSolution(str(self),
-                                            copy.deepcopy(self._factory.cache))  # todo just digits
+                try:
+                    if int(self._parts[0]) == int(self._parts[1]):
+                        yield MathRebusSolution(str(self),
+                                                {item[0]: item[1].digit
+                                                 for item in
+                                                 self._factory.cache.items()})
+                except ZeroDivisionError:
+                    continue
+                except IndexError:
+                    raise IOError('Incorrect equation.')
             else:
                 self._free[i] = False
                 self._layer += 1
--- tests.py
+++ tests.py
@@ -4,81 +4,81 @@ from mrebsolver import MathRebusSolver
 
 class MathRebusSolverTest(unittest.TestCase):
 
-    def test_common_add(self):
+    def test_add(self):
         solver = MathRebusSolver('EAT + THAT = APPLE')
         solutions = []
-        limit = 10
+        limit = 2
         for solution in solver.get_solution_generator():
-            solutions.append(solution.decoded_equation)
+            solutions.append(solution)
             limit -= 1
             if limit == 0:
                 break
-        self.assertEqual(solutions, ['819 + 9219 = 10038'])
+        self.assertEqual(len(solutions), 1)
+        self.assertEqual(solutions[0].decoded_equation, '819 + 9219 = 10038')
+        self.assertEqual(solutions[0].decoded_digits,
+                         {'E': 8, 'A': 1, 'T': 9, 'H': 2, 'P': 0, 'L': 3})
 
-    def test_common_multiply(self):
-        solver = MathRebusSolver('TWO * TWO  = THREE')
-        solutions = []
-        limit = 10
-        for solution in solver.get_solution_generator():
-            solutions.append(solution.decoded_equation)
-            limit -= 1
-            if limit == 0:
-                break
-        self.assertEqual(solutions, ['138 * 138 = 19044'])
+    def test_sub(self):
+        self.assertEqual(__class__._calculate('AB - C = 1', 2),
+                         ['10 - 9 = 1'])
+
+    def test_all_answers(self):
+        self.assertEqual(__class__._calculate('-AB + BC = AA', 10),
+                         ['- 12 + 23 = 11', '- 24 + 46 = 22', '- 36 + 69 = 33',
+                          '- 37 + 70 = 33', '- 49 + 93 = 44'])
+
+    def test_multiply(self):
+        self.assertEqual(__class__._calculate('TWO * TWO  = THREE', 2),
+                         ['138 * 138 = 19044'])
+
+    def test_division(self):
+        self.assertEqual(__class__._calculate('AB / BC = 9', 2),
+                         ['91 / 10 = 9'])
+
+    def test_brackets(self):
+        self.assertEqual(__class__._calculate(
+            '1A - (2 + (3*2) - 4/2) = B2',
+            2),
+            ['18 - ( 2 + ( 3 * 2 ) - 4 / 2 ) = 12'])
 
     def test_multiply_solutions(self):
-        solver = MathRebusSolver('A + B  = CD')
-        solutions = []
-        limit = 10
-        for solution in solver.get_solution_generator():
-            solutions.append(solution.decoded_equation)
-            limit -= 1
-            if limit == 0:
-                break
-        self.assertEqual(solutions, ['2 + 8 = 10',
-                                     '3 + 7 = 10',
-                                     '3 + 9 = 12',
-                                     '4 + 6 = 10',
-                                     '4 + 8 = 12',
-                                     '4 + 9 = 13',
-                                     '5 + 7 = 12',
-                                     '5 + 8 = 13',
-                                     '5 + 9 = 14',
-                                     '6 + 4 = 10'
-                                     ])
-
-    def test_multiple_numbers(self):
-        solver = MathRebusSolver('5 + A = 12')
-        solutions = []
-        limit = 2
-        for solution in solver.get_solution_generator():
-            solutions.append(solution.decoded_equation)
-            limit -= 1
-            if limit == 0:
-                break
-        self.assertEqual(solutions, ['5 + 7 = 12'])
+        self.assertEqual(__class__._calculate('A + B  = CD', 5),
+                         ['2 + 8 = 10', '3 + 7 = 10', '3 + 9 = 12',
+                          '4 + 6 = 10', '4 + 8 = 12'])
 
     def test_number(self):
-        solver = MathRebusSolver('NO + 4 = YES')
-        solutions = []
-        limit = 2
-        for solution in solver.get_solution_generator():
-            solutions.append(solution.decoded_equation)
-            limit -= 1
-            if limit == 0:
-                break
-        self.assertEqual(solutions, ['98 + 4 = 102'])
+        self.assertEqual(__class__._calculate('NO + 4 = YES', 2),
+                         ['98 + 4 = 102'])
+
+    def test_multiple_numbers(self):
+        self.assertEqual(__class__._calculate('5 + A = 12', 2),
+                         ['5 + 7 = 12'])
 
     def test_cyrillic(self):
-        solver = MathRebusSolver('УДАР + УДАР = ДРАКА')
-        solutions = []
-        limit = 2
+        self.assertEqual(__class__._calculate('УДАР + УДАР = ДРАКА', 2),
+                         ['8126 + 8126 = 16252'])
+
+    def test_no_result(self):
+        self.assertEqual(__class__._calculate('AB - BC = AA', 2), [])
+
+    def test_errors(self):
+        self.assertRaises(IOError, __class__._calculate, '*AB + CD = HH', 2)
+        self.assertRaises(IOError, __class__._calculate, 'AB + CD( = HH', 2)
+        self.assertRaises(IOError, __class__._calculate, 'AB + / CD = HH', 2)
+        self.assertRaises(IOError, __class__._calculate, 'AB + - CD( = HH', 2)
+        self.assertRaises(IOError, __class__._calculate, 'AB + CD = HH +', 2)
+        self.assertRaises(IOError, __class__._calculate, 'CD - AA) = HH', 2)
+
+    @staticmethod
+    def _calculate(rebus, limit):
+        solver = MathRebusSolver(rebus)
+        decoded_equations = []
         for solution in solver.get_solution_generator():
-            solutions.append(solution.decoded_equation)
+            decoded_equations.append(solution.decoded_equation)
             limit -= 1
             if limit == 0:
                 break
-        self.assertEqual(solutions, ['8126 + 8126 = 16252'])
+        return decoded_equations
 
 
 if __name__ == '__main__':
